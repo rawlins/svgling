682cbfcc8f0790832400afd58170644b6306d3a5
advil <rawlins@gmail.com>
2018-11-07 14:35:02 -0500

    Increment version number to fix pypi issues

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(name='svgling',
-      version='0.1',
-      description='SVG+Python based rendering of linguistics-style (constituent) trees',
-      author='Kyle Rawlins',
-      author_email='kgr@jhu.edu',
-      license='MIT',
-      url='https://github.com/rawlins/svgling',
-      install_requires='svgwrite',
-      python_requires='>=3',
-      packages=['svgling'],
-      classifiers=[
+    version='0.1.1',
+    description='SVG+Python based rendering of linguistics-style (constituent) trees',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Kyle Rawlins',
+    author_email='kgr@jhu.edu',
+    license='MIT',
+    url='https://github.com/rawlins/svgling',
+    install_requires='svgwrite',
+    python_requires='>=3',
+    packages=['svgling'],
+    classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Science/Research",
@@ -23,4 +31,4 @@ setup(name='svgling',
         "Topic :: Utilities",
         "Framework :: Jupyter",
         "Environment :: Web Environment"]
-     )
+    )
