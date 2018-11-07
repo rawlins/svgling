f768889d93a94376638a96893a66f49e7a6ab9c4
advil <rawlins@gmail.com>
2018-11-06 23:07:56 -0500

    Add a setup.py
    In case people want to like, install this.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='svgling',
+      version='0.0.1',
+      description='svgling syntax tree renderer',
+      author='Kyle Rawlins',
+      author_email='kgr@jhu.edu',
+      url='https://github.com/rawlins/svgling',
+      packages=['svgling'],
+     )
